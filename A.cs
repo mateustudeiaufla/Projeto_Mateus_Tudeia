3002b7481671cc74a0bee5a68f820fa67661f863
Mateus Tudeia <mateus.tudeia@estudante.ufla.br>
2020-06-09 22:28:25 -0300

    Criação das classes A e B

---
+++ A.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class A
+{
+    public int A1 { get; set; }
+
+    public float A2 { get; set; }
+
+    public A()
+	{
+	}
+
+	public void MA1()
+    {
+		Console.WriteLine("MA1");
+    }
+	public void MA2()
+    {
+		Console.WriteLine("MA2");
+    }
+}
---
+++ B.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class B
+{
+	public float B1 { get; set; }
+
+	public float B2 { get; set; }
+
+    public B()
+	{
+		
+	}
+
+	public void MB1()
+	{
+		Console.WriteLine("MB1");
+	}
+	public void MB2()
+	{
+		Console.WriteLine("MB2");
+	}
+}
