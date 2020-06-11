58464e494014bc2f7bde644b997742fa7127f095
mateustudeia <mateus.tudeia@estudante.ufla.br>
2020-06-10 23:01:58 -0300

    Criação da Classe C

---
+++ C.cs
@@ -0,0 +1,22 @@
+using System;
+
+public class C
+{
+	public string C1 { get; set; }
+
+	public int C2 { get; set; }
+
+    public C()
+	{
+		
+	}
+
+	public void MC1()
+	{
+		Console.WriteLine("MC1");
+	}
+	public void MC2()
+	{
+		Console.WriteLine("MC2");
+	}
+}
